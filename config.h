74869165887fd5df86ebf7060f947f28d42ef3b1
leejkee <leejykee@yeah.net>
2022-03-22 10:16:56 +0800

    patch the barpadding

--- config.h
+++ config.h
@@ -6,6 +6,8 @@ static const unsigned int gappx = 6;/* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 10;        /* vertical padding of bar */
+static const int sidepad            = 10;        /* horizontal padding of bar */
 static const char *fonts[]          = { "Source Code Pro:size=24", 
 					"Symbols Nerd Font:size=24:type:2048-em", 
 					"WenQuanYi Micro Hei:size=24:type=Rugular" };
@@ -29,7 +31,7 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
